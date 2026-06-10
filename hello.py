fa94b4f6f014da744f819a95e761810fd723b182
Nathan <nathan.monar@etu.u-pec.fr>
2026-06-10 10:42:27 +0200

    correction du fichier hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from colorama import init, Fore, Style
 init()
 
 def display():
-    message = f"{Fore.GREEN}Bonjour les {Fore.CYAN}FI1{Style.RESET_ALL}"
+    message = f"{Fore.GREEN}Ceci est une {Fore.CYAN}modification{Style.RESET_ALL}"
     print(message)
 
 if __name__ == "__main__":
